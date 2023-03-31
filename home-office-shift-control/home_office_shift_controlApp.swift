4cf7d40ac96044f6ff18343e69e2237f52a1bbb6
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2023-03-31 04:39:20 -0300

    crud finalizado em api

--- home-office-shift-control/home_office_shift_controlApp.swift
+++ home-office-shift-control/home_office_shift_controlApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct home_office_shift_controlApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(ViewModel())
         }
     }
 }
