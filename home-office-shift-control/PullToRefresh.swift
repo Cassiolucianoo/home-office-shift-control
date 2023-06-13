7a7544e923165e39d041e7893f30e8b1ad88e4e8
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2023-06-13 05:44:58 -0300

    View edit, add, home concluidas

---
+++ home-office-shift-control/PullToRefresh.swift
@@ -0,0 +1,20 @@
+//
+//  PullToRefresh.swift
+//  home-office-shift-control
+//
+//  Created by cassio on 13/06/23.
+//
+
+import SwiftUI
+
+struct PullToRefresh: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PullToRefresh_Previews: PreviewProvider {
+    static var previews: some View {
+        PullToRefresh()
+    }
+}
